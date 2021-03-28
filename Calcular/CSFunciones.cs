f84784a4bebc579544796e4df52bfdf4f6b0e4e0
tfpaula.acera@hotmail.com <tfpaula.acera@hotmail.com>
2021-03-28 12:55:08 +0200

    Agregar archivos de proyecto.

---
+++ Calcular/CSFunciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular
+{
+    class CSFunciones
+    {
+        public static int Suma(int operador1, int operador2)
+        {
+            int suma = operador1 + operador2;
+            return (suma);
+        }
+        public static int Multiplicacion(int operador1, int operador2)
+        {
+            int multiplicacion = operador1 * operador2;
+            return (multiplicacion);
+        }
+        public static void MostrarResultados(int operador1, int operador2, int suma, int resta, int multiplicacion, int division)
+        {
+            Console.WriteLine("El resultado de la suma {0} y {1} es {2}", operador1, operador2, suma); // Modificado por Paula
+            Console.WriteLine("El resultado de la multiplicacion {0} y {1} es {2}", operador1, operador2, multiplicacion); // Modificado por Paula
+        }
+    }
+}
---
+++ Calcular/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto de cálculo");
+            int operador1 = 10;
+            int operador2 = 5;
+            // int suma = CSFunciones.Suma(operador1, operador2);
+            // int resta = CSFunciones.Resta(operador1, operador2);
+            // int multiplicacion = CSFunciones.Multiplicacion(operador1, operador2);
+            // int division = CSFunciones.Division(operador1, operador2);
+            Console.Write("Pulsa una tecla para finalizar");
+            Console.ReadKey(true);
+        }
+    }
+}
